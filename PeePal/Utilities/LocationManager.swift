bf1ab4409ea87d34f6818feb58a2eb8a056f1baa
tgpatrick <thomasgpatrick0203@gmail.com>
2026-03-28 15:27:40 -0400

    Add Liquid Glass icon

---
+++ PeePal/Utilities/LocationManager.swift
@@ -0,0 +1,46 @@
+//
+//  LocationManager.swift
+//  PeePal
+//
+//  Created by Thomas Patrick on 11/13/20.
+//
+
+import Foundation
+import CoreLocation
+
+@Observable
+class LocationManager: NSObject {
+    static let shared = LocationManager()
+    private let locationManager = CLLocationManager()
+    var location: CLLocation?
+    var locationError: Error?
+
+    override init() {
+        super.init()
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+
+    func requestLocation() {
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+
+    func distance(to coordinate: CLLocationCoordinate2D) -> CLLocationDistance? {
+        requestLocation()
+        guard let location else { return nil }
+        let coordinateCLLocation = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+
+        return location.distance(from: coordinateCLLocation)
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.last
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationError = error
+    }
+}
